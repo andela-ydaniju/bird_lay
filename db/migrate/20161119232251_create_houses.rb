f74a130bc63dc36ead9dcdac88a27bbb4814785e
andela-ydaniju <yusuf.daniju@andela.com>
2016-11-20 00:26:51 +0100

    add bird features

--- db/migrate/20161119232251_create_houses.rb
+++ db/migrate/20161119232251_create_houses.rb
@@ -1,6 +1,10 @@
 class CreateHouses < ActiveRecord::Migration[5.0]
   def change
     create_table :houses do |t|
+      t.string :name
+      t.string :code
+      t.integer :capacity
+      t.string :stage
 
       t.timestamps
     end
