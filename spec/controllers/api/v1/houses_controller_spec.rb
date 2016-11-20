cd52e18e1631c8d51fd81a1aa4c0bd7fb7de3ada
andela-ydaniju <yusuf.daniju@andela.com>
2016-11-20 07:00:00 +0100

    index page for houses

--- spec/controllers/api/v1/houses_controller_spec.rb
+++ spec/controllers/api/v1/houses_controller_spec.rb
@@ -2,9 +2,15 @@ require 'rails_helper'
 
 RSpec.describe Api::V1::HousesController, type: :controller do
   context '#index' do
+    let(:admin) { create :admin }
+    before(:each) do
+      create_list(:house, 3, admin_id: admin.id)
+    end
     it 'renders all houses' do
       get :index
 
+      body = JSON.parse(response.body)
+      expect(body.count).to eql 3
       expect(response).to have_http_status(200)
     end
   end
