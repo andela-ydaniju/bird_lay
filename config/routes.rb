2fa096453a3b0a712f3bb7ad2419ba1080628370
andela-ydaniju <yusuf.daniju@andela.com>
2016-11-21 10:48:32 +0100

    remove constraints in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  constraints format: 'json' do
-    root to: 'home#index'
-    constraints subdomain: 'api' do
-      scope module: 'api' do
-        namespace :v1 do
-          resources :houses
-        end
-      end
+  root to: 'home#index'
+  scope module: 'api' do
+    namespace :v1 do
+      resources :houses
     end
   end
 end
