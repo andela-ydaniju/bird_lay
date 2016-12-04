3a560faa26d3b88e448c1d983cf37609f940a245
andela-ydaniju <yusuf.daniju@andela.com>
2016-12-04 21:03:39 +0100

    create pen houses

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -1,6 +1,31 @@
 class HousesController < ApplicationController
   before_action :login_required!
+  def index
+    @houses = House.all
+  end
+
   def new
     @house = current_user.houses.new
+  end
+
+  def create
+    @house = current_user.houses.new(house_params)
+
+    if @house.save
+      redirect_to house_path(@house.id)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @house = House.find_by(id: params[:id])
+  end
+
+  private
+  def house_params
+    params.require(:house).permit(
+      :name, :code, :capacity, :population, :feed_consumption
+    )
   end
 end
