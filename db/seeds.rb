54d9ba33d2d34e766870aa5d22bb9781a6e9ff20
andela-ydaniju <yusuf.daniju@andela.com>
2016-12-04 21:41:29 +0100

    populated froms seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,16 @@
-User.create(
+user = User.create(
   name: 'Admin',
   email: 'admin@birdlay.com',
   password: 'password',
   level: 2
 )
+
+4.times do
+  user.houses.create(
+    name: FFaker::Name.first_name,
+    code: FFaker::Identification.ssn,
+    capacity: 54000,
+    population: 34000,
+    feed_consumption: 230.45
+  )
+end
