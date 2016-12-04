4208f8580dd795fd251bbceae6533453c9c82cf8
andela-ydaniju <yusuf.daniju@andela.com>
2016-12-04 12:23:34 +0100

    handle sessions

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,30 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      flash.now[:success] = "Successfully signed in"
+      sign_in user
+      remember user
+      session[:id] = user.id
+      redirect_to current_user
+    else
+      flash.now[:danger] = "Invalid email/password combination"
+      render "new"
+    end
+  end
+
+  def destroy
+    sign_out if signed_in?
+    redirect_to root_url
+  end
+
+  private
+
+  def current_user
+    user_id = session[:user_id]
+    @current_user ||= User.find_by(id: user_id)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   root to: 'home#index', as: :home
 
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
+  get 'signin', to: 'sessions#new', as: :signin
 end
