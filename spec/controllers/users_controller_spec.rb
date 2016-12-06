8748f28de9cfa128806b0921f410213fd75215e8
andela-ydaniju <yusuf.daniju@andela.com>
2016-12-06 07:16:54 +0100

    test for user creation

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -26,18 +26,30 @@ RSpec.describe UsersController, type: :controller do
   describe '#create' do
     context 'when not signed in' do
       it 'redirects to root' do
-        get :new
+        post :create, params: { email: new_user.email, password: new_user.password }
 
         expect(response).to redirect_to(root_url)
       end
     end
 
     context 'when signed in' do
-      it 'renders page for creating new user' do
+      it 'creates no user when params are incorrect' do
+        new_user.email = nil
         login(user)
-        get :new, session: { user_id: user.id }
 
-        expect(response).to render_template(:new)
+        expect {
+          post :create, params: { user: { email: new_user.email, password: new_user.password }},
+          session: { user_id: user.id }
+        }.to change(User, :count).by 0
+      end
+
+      it 'creates new user when all params correct' do
+        login(user)
+        new_user.email = 'new@user.email'
+        expect {
+          post :create, params: { user: { email: new_user.email, password: new_user.password }},
+          session: { user_id: user.id }
+        }.to change(User, :count).by 1
       end
     end
   end
